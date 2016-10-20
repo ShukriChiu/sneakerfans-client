4263c136022f7e9b32f81e196502677a96de85e6
ShukriChiu <shukrichiu@foxmail.com>
2016-10-20 01:03:28 -0400

    add
    Signed-off-by: ShukriChiu <shukrichiu@foxmail.com>

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-var express = require('express');
-var path = require('path');
+var express = require('express'),
+    path = require('path'),
+    app = express();
+
 app = express();
 app.use('/dist', express.static('dist'));
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
+    res.sendFile(path.join(__dirname + '/views/index.html'));
 });
 var port = process.env.PORT || 3000;
-app.listen(port);
-console.log('server started:' + port);
+app.listen(port, console.log('server started:' + port));
--- src/App.vue
+++ src/App.vue
@@ -24,13 +24,13 @@ export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Vue.js'
     }
   }
 }
 </script>
 
-<style>
+<style scoped>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -41,7 +41,7 @@ export default {
 }
 
 h1, h2 {
-  font-weight: normal;
+  font-weight: 1.2em;
 }
 
 ul {
